4c6e02407ae4088697e37340872dd0681d927404
rishab <rkc3660@gmail.com>
2023-07-02 16:02:45 +0530

    fixed rate limiter error handling bug

--- Shyaari_Generator/index.js
+++ Shyaari_Generator/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const limiter = rateLimit({
-	windowMs: 3 * 60 * 1000, // 5 minutes
+	windowMs: 5 * 60 * 1000, // 5 minutes
 	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
